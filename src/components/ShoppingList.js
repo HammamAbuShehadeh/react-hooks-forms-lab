b13765cdfe08d1e2f43ea5b92038917fad752789
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2024-03-13 01:28:20 +0300

    Completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,22 +4,48 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  const [formData, setFormData] = useState({
+    filterByCategory: "All",
+    searchByName: "",
+    addItem: "",
+    addCategory: "Produce"
+  })
+  const [addItemsToList, setAddItemsToList] = useState([...items])
+  
+  function handleSubmit(itemsArray){
+     setAddItemsToList(() => [...addItemsToList, itemsArray])
+  
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleChange(event){
+    const name = event.target.name
+    let value = event.target.value
 
-    return item.category === selectedCategory;
+    setFormData({
+        ...formData,
+         [name]: value,
+        })
+  }
+
+  let itemsToDisplay = addItemsToList.filter((item) => {
+    
+    if (formData.filterByCategory === "All") return true;
+    else 
+    return item.category === formData.filterByCategory;
   });
 
+  if(formData.searchByName !== ""){
+    itemsToDisplay = addItemsToList.filter((item) => {
+    
+    
+      return item.name.toLowerCase().includes(formData.searchByName.toLowerCase())
+  }) }
+  
+ 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm handleChange={handleChange} addItemValue={formData.addItem} addCategoryValue={formData.addCategory} onItemFormSubmit={handleSubmit}/>
+      <Filter onSearchChange={handleChange} search={formData.searchByName} categoryValue={formData.filterByCategory}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
